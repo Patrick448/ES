529f1f7801235e299d5d30b9bed0b3086b8a53d8
Patrick448 <patrickcarvalho448@gmail.com>
2023-01-30 10:04:32 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,23 @@
 // Created by patri on 27/01/2023.
 //
 #include <iostream>
+#include <fstream>
 #include <math.h>
 #include "ESAlgorithm.h"
 using namespace std;
 
+void outputToFile(string path, string text, bool append){
+    ofstream outputf;
+
+    if(append){
+        outputf.open(path, std::ios_base::app);
+    }else{
+        outputf.open(path);
+    }
+
+    outputf << text;
+    outputf.close();
+}
 
 double testFunc(Individual* ind){
     double eval =0;
@@ -28,15 +41,15 @@ int main(){
 
    esAlgorithm.setEvaluationFunction(testFunc);
    esAlgorithm.run1Plus1ES(1, 1.0, 0.817, 10, 250);
-   cout << esAlgorithm.populationToString() + "\n";
+   cout << esAlgorithm.populationToCSVString() + "\n";
 
     esAlgorithm.setSigmaBounds(0.1, 10);
     esAlgorithm.runPopulationalIsotropicES(1, 0.5, 100, 5, 10);
-    cout << esAlgorithm.populationToString() + "\n";
+    cout << esAlgorithm.populationToCSVString() + "\n";
 
     esAlgorithm.setSigmaBounds(0.1, 10);
     esAlgorithm.runPopulationalNonIsotropicES(1, 0.5, 100, 5, 10);
-    cout << esAlgorithm.populationToString() + "\n";
+    cout << esAlgorithm.populationToCSVString() + "\n";
 
     cout << "\nFinished\n";
 }
