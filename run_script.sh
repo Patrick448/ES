1d616e0e8f5be55e82529ba3f91ce7bef91124d6
Patrick Carvalho <patrickcarvalho448@gmail.com>
2023-11-04 15:35:43 -0300

    modifica run script

--- run_script.sh
+++ run_script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 for i in {0..29}
 do
-   ./build/ES grn5 lsoda cmaes 105000 $i >> output.txt &
+   ./build/ES grn5 lsoda cmaes 1050000 $i >> output5-testing-limits.txt &
 done
 
 exit
+
