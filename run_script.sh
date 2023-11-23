38c6751bb5038bbb0e8f8d0047857cebca04dc8c
Patrick Carvalho <patrickcarvalho448@gmail.com>
2023-11-23 12:44:12 -0300

    altera script

--- run_script.sh
+++ run_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for i in {0..29}
 do
-   ./build/ES grn5 lsoda de 1050000 $i >> de-output5-old-limits.txt &
+   ./build/ES grn10 lsoda de 1050000 $i >> de-output10-old-limits.txt &
 done
 
 exit
