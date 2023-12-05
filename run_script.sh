a2df1ba515cf67a3f8e4369655fbb5f3eec05a4a
Patrick Carvalho <patrickcarvalho448@gmail.com>
2023-12-05 14:43:05 -0300

    altera script e muda limites

--- run_script.sh
+++ run_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for i in {0..29}
 do
-   ./build/ES grn5 lsoda sade 1050000 $i >> sade-output5-old-limits.txt &
+   ./build/ES grn10 lsoda sade 1050000 $i >> sade-output5-new-limits.txt &
 done
 
 exit
