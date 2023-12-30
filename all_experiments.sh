61a0eb1b21d86adfde5d1cf5a589504c3fd12a2b
Patrick Carvalho <patrickcarvalho448@gmail.com>
2023-12-30 19:02:06 -0300

    modifica script

--- all_experiments.sh
+++ all_experiments.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 BUDGET=1050000
-INPUT_FILE="GRN5.txt"
+INPUT_FILE="GRN10.txt"
 MODEL="grn10"
 EXE_PATH="./build/ES"
 EXP_NAME="$MODEL-$BUDGET"
 
 mkdir $EXP_NAME
-./batch_run.sh 4 30 "$EXE_PATH -i GRN5.txt -m $MODEL -e lsoda -a cmaes -n $BUDGET -s \$i >> $EXP_NAME/output-$MODEL-cmaes.txt"
-./batch_run.sh 4 30 "$EXE_PATH -i GRN5.txt -m $MODEL -e lsoda -a de -n $BUDGET -s \$i >> $EXP_NAME/output-$MODEL-de.txt"
-./batch_run.sh 4 30 "$EXE_PATH -i GRN5.txt -m $MODEL -e lsoda -a es-i -n $BUDGET -s \$i >> $EXP_NAME/output-$MODEL-es-i.txt"
-./batch_run.sh 4 30 "$EXE_PATH -i GRN5.txt -m $MODEL -e lsoda -a es-ni -n $BUDGET -s \$i >> $EXP_NAME/output-$MODEL-es-ni.txt"
+./batch_run.sh 4 30 "$EXE_PATH -i $INPUT_FILE -m $MODEL -e lsoda -a cmaes -n $BUDGET -s \$i >> $EXP_NAME/output-$MODEL-cmaes.txt"
+./batch_run.sh 4 30 "$EXE_PATH -i $INPUT_FILE -m $MODEL -e lsoda -a de -n $BUDGET -s \$i >> $EXP_NAME/output-$MODEL-de.txt"
+./batch_run.sh 4 30 "$EXE_PATH -i $INPUT_FILE -m $MODEL -e lsoda -a es-i -n $BUDGET -s \$i >> $EXP_NAME/output-$MODEL-es-i.txt"
+./batch_run.sh 4 30 "$EXE_PATH -i $INPUT_FILE -m $MODEL -e lsoda -a es-ni -n $BUDGET -s \$i >> $EXP_NAME/output-$MODEL-es-ni.txt"
