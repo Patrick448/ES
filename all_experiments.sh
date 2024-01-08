f503394eaf3e28866052f9226c5ed67f65aa1840
Patrick Carvalho <patrickcarvalho448@gmail.com>
2024-01-08 16:59:23 -0300

    altera script

--- all_experiments.sh
+++ all_experiments.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 BUDGET=1050000
-INPUT_FILE="GRN10_DATA.txt"
-MODEL="grn10new"
+INPUT_FILE="DadosZeEduardo_DATA.txt"
+MODEL="grn5ncyc"
 EXE_PATH="./build/ES"
-DIFF="n0"
+DIFF=""
 EXP_NAME="$MODEL-$BUDGET-$DIFF"
-SET_DIV="-sd 0 34 35 49"
+SET_DIV="-sd 0 113 114 161"
 
 mkdir $EXP_NAME
 ./batch_run.sh 4 30 "$EXE_PATH -i $INPUT_FILE -m $MODEL -e lsoda -a cmaes -n $BUDGET -s \$i $SET_DIV >> $EXP_NAME/output-$MODEL-cmaes.txt"
