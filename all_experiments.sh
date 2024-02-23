15bfe089e264c5e1d4b65b190a6c63db11457344
Patrick Carvalho <patrickcarvalho448@gmail.com>
2024-02-23 17:37:32 -0300

    adiciona script

--- all_experiments.sh
+++ all_experiments.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-BUDGET=1050000
-INPUT_FILE="DadosZeEduardo_DATA.txt"
-MODEL="grn5ncyc"
-EXE_PATH="./build/ES"
-DIFF="test"
-EXP_NAME="$MODEL-$BUDGET-$DIFF"
-SET_DIV="-sd 0 113 114 161"
-TEST_SET=""
-
-mkdir $EXP_NAME
-./batch_run.sh 4 30 "$EXE_PATH -i $INPUT_FILE $TEST_SET -m $MODEL -e lsoda -a cmaes -n $BUDGET -s \$i $SET_DIV >> $EXP_NAME/output-$MODEL-cmaes.csv"
-./batch_run.sh 4 30 "$EXE_PATH -i $INPUT_FILE $TEST_SET -m $MODEL -e lsoda -a de -n $BUDGET -s \$i $SET_DIV >> $EXP_NAME/output-$MODEL-de.csv"
-./batch_run.sh 4 30 "$EXE_PATH -i $INPUT_FILE $TEST_SET -m $MODEL -e lsoda -a sade -n $BUDGET -s \$i $SET_DIV >> $EXP_NAME/output-$MODEL-sade.csv"
-./batch_run.sh 4 30 "$EXE_PATH -i $INPUT_FILE $TEST_SET -m $MODEL -e lsoda -a es-i -n $BUDGET -s \$i $SET_DIV >> $EXP_NAME/output-$MODEL-es-i.csv"
-./batch_run.sh 4 30 "$EXE_PATH -i $INPUT_FILE $TEST_SET -m $MODEL -e lsoda -a es-ni -n $BUDGET -s \$i $SET_DIV >> $EXP_NAME/output-$MODEL-es-ni.csv"
+./all_experiments_grn5.sh
+./all_experiments_grn10.sh
+./all_experiments_abcd.sh
+./all_experiments_trpEcoli.sh
